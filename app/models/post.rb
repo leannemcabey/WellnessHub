cdbb8e1c84e7b60cad53ce68389edfdeca6b65e5
Skylar Salerno <skylar.salerno@gmail.com>
2018-12-03 17:54:02 -0500

    fix-index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :categories,through: :post_categories
   belongs_to :owner
   has_many :user_posts
   has_many :posts, through: :user_posts
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :user_intentions
+  has_many :intentions
   has_many :owned_posts, foreign_key: "owner_id", class_name: "Post"
   has_many :user_posts
   has_many :posts, through: :user_posts
