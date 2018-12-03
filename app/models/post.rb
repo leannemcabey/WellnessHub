2ff79399c7c068321f5f5b1f6793c44e275c330f
leannemcabey <leanne.m.cabey@gmail.com>
2018-12-03 17:48:09 -0500

    Update schema and add CRUD to users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :post_categories
   has_many :categories, through: :post_categories
-  belongs_to :owner
+  belongs_to :owner, class_name: 'User'
   has_many :user_posts
   has_many :posts, through: :user_posts
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :user_intentions
-  resources :user_posts
-  resources :post_categories
+  resources :user_intentions, only: [:destroy]
+  resources :user_posts, only: [:create]
+  resources :post_categories, only: [:create]
   resources :posts
-  resources :categories
-  resources :users
+  # resources :categories
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
