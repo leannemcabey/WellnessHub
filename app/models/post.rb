6eb424c40bd16d5473f191bd73f3a97ad5d60424
leannemcabey <leanne.m.cabey@gmail.com>
2018-12-03 15:38:29 -0500

    add category_attributes setter method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
   belongs_to :owner
   has_many :user_posts
   has_many :posts, through: :user_posts
+
+  def category_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find(category_attribute)
+      self.categories << category
+    end
+  end
+
 end
--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -2,4 +2,5 @@ class UserPost < ApplicationRecord
   belongs_to :post
   belongs_to :user
   belongs_to :intention
+
 end
